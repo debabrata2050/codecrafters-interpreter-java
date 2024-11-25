429fb25204d7099b349884c32f267460760fca1f
Debabrata Halder <99135144+debabrata2050@users.noreply.github.com>
2024-11-25 22:27:38 +0530

    uncomment it

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,8 +4,8 @@ import java.nio.file.Path;
 
 public class Main {
   public static void main(String[] args) {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    System.err.println("Logs from your program will appear here!");
+    // // You can use print statements as follows for debugging, they'll be visible when running tests.
+    // System.err.println("Logs from your program will appear here!");
 
     if (args.length < 2) {
       System.err.println("Usage: ./your_program.sh tokenize <filename>");
@@ -30,10 +30,10 @@ public class Main {
 
     // Uncomment this block to pass the first stage
     // 
-    // if (fileContents.length() > 0) {
-    //   throw new RuntimeException("Scanner not implemented");
-    // } else {
-    //   System.out.println("EOF  null"); // Placeholder, remove this line when implementing the scanner
-    // }
+    if (fileContents.length() > 0) {
+      throw new RuntimeException("Scanner not implemented");
+    } else {
+      System.out.println("EOF  null"); // Placeholder, remove this line when implementing the scanner
+    }
   }
 }
