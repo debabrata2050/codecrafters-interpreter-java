4be4d8d7b29c311cb5d484602fce72a41a423851
Debabrata Halder <99135144+debabrata2050@users.noreply.github.com>
2024-11-28 19:59:26 +0530

    number literals

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,13 +3,16 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class Main {
-
+  
   private static String removeTrailingZeroes(String s) {
     int index;
     for (index = s.length() - 1; index >= 0; index--) {
       if (s.charAt(index) != '0') {
         break;
       }
+    }
+    if (index == 0 && s.charAt(index) == '.') {
+      return "";
     }
     return s.substring(0, index + 1);
   }
